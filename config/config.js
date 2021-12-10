9e0765f2f69fe38158159c3601f54b9556c6694c
Khurshid Ismoilov <94281113+khurshid2827@users.noreply.github.com>
2021-12-10 23:56:07 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const config ={
+    MongoDB_URL:"mongodb+srv://khurshid:khurshid2827@khurshidcluster.ae2lf.mongodb.net/QazoNamozlar?retryWrites=true&w=majority"
+}
+
+module.exports={
+    config
+}
