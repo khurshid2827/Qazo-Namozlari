d84f45f29f9984c050ea15c86b10a1c991c7faaf
Khurshid Ismoilov <94281113+khurshid2827@users.noreply.github.com>
2021-12-10 23:36:56 +0300

    Update server.js

--- core/server.js
+++ core/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port =process.env.PORT 3000
+const port =process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello My Team!'))
+app.get('/', (req, res) => res.send('Welcome to Qazo Namozlari App'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports={
