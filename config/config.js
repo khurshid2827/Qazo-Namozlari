2dbabaa2fd9d54abee7d9504dc6f3e4d25e79beb
Khurshid Ismoilov <94281113+khurshid2827@users.noreply.github.com>
2021-12-11 14:32:20 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,3 @@ const config ={
     MongoDB_URL:"mongodb+srv://khurshid:khurshid2827@khurshidcluster.ae2lf.mongodb.net/QazoNamozlar?retryWrites=true&w=majority"
 }
 
-module.exports={
-    config
-}
